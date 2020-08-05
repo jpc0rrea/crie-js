c6a238156b609039b04920b38b7c8c5c6a63c62d
jpc0rrea <joaopedrocorrea14@gmail.com>
2020-08-05 18:48:57 -0300

    Style Home
    Co-authored-by: lucaseverest <lucaseverest10@gmail.com>
    Co-authored-by: rodrigozcipriano <rodrigozcipriano1@gmail.com>

--- crie-client/src/pages/Login/index.js
+++ crie-client/src/pages/Login/index.js
@@ -3,7 +3,32 @@ import React from "react";
 import "./styles.css";
 
 function Login() {
-  return <h1>Login</h1>;
+  return (
+    <div id="page-login">
+      <div id="page-login-content">
+        <div id="left-side">
+          <div className="welcome-texts">
+            <h1 className="page-title">Crie</h1>
+            <h1>Bem vindo!</h1>
+            <p>Faça login para entrar no sistema de inovação da sua empresa!</p>
+          </div>
+          <form className="login-form">
+            <div className="input-block">
+              <input type="text" id="email" placeholder="E-mail" />
+            </div>
+
+            <div className="input-block">
+              <input type="password" id="password" placeholder="Senha" />
+            </div>
+
+            <a href="/">Esqueci minha senha</a>
+
+            <button type="submit">Entrar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
